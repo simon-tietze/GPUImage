85cc660afe0cab8d5c7279ffcdd9fe0461a190f1
Simon Tietze <fqh786@motorola.com>
2013-09-15 14:29:42 -0700

    logging

--- examples/iOS/RawDataTest/RawDataTest/RawDataTestAppDelegate.m
+++ examples/iOS/RawDataTest/RawDataTest/RawDataTestAppDelegate.m
@@ -6,15 +6,16 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    unsigned char n = 1000;
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
-    GLubyte *rawDataBytes = calloc(10 * 10 * 4, sizeof(GLubyte));
-    for (unsigned int yIndex = 0; yIndex < 10; yIndex++)
+    GLubyte *rawDataBytes = calloc(n * n * 4, sizeof(GLubyte));
+    for (unsigned int yIndex = 0; yIndex < n; yIndex++)
     {
-        for (unsigned int xIndex = 0; xIndex < 10; xIndex++)
+        for (unsigned int xIndex = 0; xIndex < n; xIndex++)
         {
             rawDataBytes[yIndex * 10 * 4 + xIndex * 4] = xIndex;
             rawDataBytes[yIndex * 10 * 4 + xIndex * 4 + 1] = yIndex;
@@ -23,9 +24,9 @@
         }
     }
     
-    GPUImageRawDataInput *rawDataInput = [[GPUImageRawDataInput alloc] initWithBytes:rawDataBytes size:CGSizeMake(10.0, 10.0)];    
+    GPUImageRawDataInput *rawDataInput = [[GPUImageRawDataInput alloc] initWithBytes:rawDataBytes size:CGSizeMake(n, n)];
     GPUImageFilter *customFilter = [[GPUImageFilter alloc] initWithFragmentShaderFromFile:@"CalculationShader"];
-    GPUImageRawDataOutput *rawDataOutput = [[GPUImageRawDataOutput alloc] initWithImageSize:CGSizeMake(10.0, 10.0) resultsInBGRAFormat:YES];
+    GPUImageRawDataOutput *rawDataOutput = [[GPUImageRawDataOutput alloc] initWithImageSize:CGSizeMake(n, n) resultsInBGRAFormat:YES];
     
     [rawDataInput addTarget:customFilter];
     [customFilter addTarget:rawDataOutput];
@@ -34,9 +35,9 @@
         GLubyte *outputBytes = [rawDataOutput rawBytesForImage];
         NSInteger bytesPerRow = [rawDataOutput bytesPerRowInOutput];
         NSLog(@"Bytes per row: %d", bytesPerRow);
-        for (unsigned int yIndex = 0; yIndex < 10; yIndex++)
+        for (unsigned int yIndex = 0; yIndex < n; yIndex++)
         {
-            for (unsigned int xIndex = 0; xIndex < 10; xIndex++)
+            for (unsigned int xIndex = 0; xIndex < n; xIndex++)
             {
                 NSLog(@"Byte at (%d, %d): %d, %d, %d, %d", xIndex, yIndex, outputBytes[yIndex * bytesPerRow + xIndex * 4], outputBytes[yIndex * bytesPerRow + xIndex * 4 + 1], outputBytes[yIndex * bytesPerRow + xIndex * 4 + 2], outputBytes[yIndex * bytesPerRow + xIndex * 4 + 3]);
             }
