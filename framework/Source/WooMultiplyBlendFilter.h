4f954478f59a10d4735ddbc37f9a3531eb29f04c
Simon Tietze <fqh786@motorola.com>
2013-11-09 17:55:19 -0800

    added settings

--- framework/Source/WooMultiplyBlendFilter.h
+++ framework/Source/WooMultiplyBlendFilter.h
@@ -4,11 +4,19 @@
 {
 	GPUMatrix4x4 colorTransform;
 	GLint colorTransformUniform;
+	
+	GLint lumaPowerUniform;
+	GLint lumaMultUniform;
+	GLint lumaBaseUniform;
+	GLint gammaUniform;
 }
 
 @property (readwrite, nonatomic, copy) UIColor* color1;
 @property (readwrite, nonatomic, copy) UIColor* color2;
 @property (readwrite, nonatomic, copy) UIColor* color3;
-@property (readwrite, nonatomic) CGFloat luminanceTransfer;
+@property (readwrite, nonatomic) CGFloat lumaPower;
+@property (readwrite, nonatomic) CGFloat lumaMult;
+@property (readwrite, nonatomic) CGFloat lumaBase;
+@property (readwrite, nonatomic) CGFloat gamma;
 
 @end
